8f474fb03cfe3176c690c7dffb540d1dc61b099b
wangzhen <h_wangzhen@wps.cn>
2024-02-22 19:26:31 +0800

    配置方案的实现

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"yftxhub/bootstrap"
+	btsConfig "yftxhub/config"
+	"yftxhub/pkg/config"
 
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
+
 func main() {
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .envtesting 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
 	// 初始化 Gin 实例
 	r := gin.New()
 
@@ -15,7 +29,7 @@ func main() {
 	bootstrap.SetupRoute(r)
 
 	// 运行服务
-	err := r.Run(":80")
+	err := r.Run(":" + config.Get("app.port"))
 	if err != nil {
 		// 错误处理，端口被占用了或者其他错误
 		fmt.Println(err.Error())
