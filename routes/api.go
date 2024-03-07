270cebd4abd994b6d4c175b9a7b0223e7c40031e
wangzhen <h_wangzhen@wps.cn>
2024-03-07 17:26:50 +0800

    Email + 邮件验证码注册

--- routes/api.go
+++ routes/api.go
@@ -21,6 +21,7 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			// 判断手机号是否注册
 			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
 			authGroup.POST("/signup/using-phone", suc.SignupUsingPhone)
+			authGroup.POST("/signup/using-email", suc.SignupUsingEmail)
 
 			// 发送验证码
 			vcc := new(auth.VerifyCodeController)
