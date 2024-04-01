f2d83f3151f162e5975a030dfcfe71b12b9fd64c
wangzhen <h_wangzhen@wps.cn>
2024-04-01 12:02:10 +0800

    migrate rollback 命令

--- app/cmd/make/make_migration.go
+++ app/cmd/make/make_migration.go
@@ -12,7 +12,7 @@ var CmdMakeMigration = &cobra.Command{
 	Use:   "migration",
 	Short: "Create a migration  file, example: make migration add_users_table",
 	Run:   runMakeMigration,
-	Args:  cobra.ExactArgs(1), // 只允许且必须传一个参数
+	Args:  cobra.ExactArgs(2), // 只允许且必须传一个参数
 }
 
 func runMakeMigration(cmd *cobra.Command, args []string) {
@@ -21,9 +21,10 @@ func runMakeMigration(cmd *cobra.Command, args []string) {
 
 	// 格式化模型名称,返回一个 Model 对象
 	model := makeModelFromString(args[0])
+	st := makeModelFromString(args[1])
 	fileName := timeStr + "_" + model.PackageName
 	filePath := fmt.Sprintf("database/migrations/%s.go", fileName)
-	createFileFromStub(filePath, "migration", model, map[string]string{"{{FileName}}": fileName})
+	createFileFromStub(filePath, "migration", st, map[string]string{"{{FileName}}": fileName})
 	console.Success("Migration file created, after modify it, use `migrate up` to migration database.")
 
 }
